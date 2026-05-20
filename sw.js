1b1bc1c697282942a8804e410ac829d7e0bf55e8
elyeshs <elyeshs@gmail.com>
2026-05-20 18:56:47 +0200

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,3 @@
+const CACHE = 'health-tracker-v2';
+self.addEventListener('install', (e) => e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', './index.html', './style.css', './app.js', './manifest.json']))));
+self.addEventListener('fetch', (e) => e.respondWith(caches.match(e.request).then(res => res || fetch(e.request))));
